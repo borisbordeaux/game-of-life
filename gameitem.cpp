949b312fdff59ef85d72a888d9e32c56322b0247
Boris <boris.bordeaux@gmail.com>
2024-02-21 18:12:13 +0100

    Added grid drawing for the gameitem

--- gameitem.cpp
+++ gameitem.cpp
@@ -3,24 +3,31 @@
 
 #include <QPainter>
 
-GameItem::GameItem(const Game &game, qreal cellSize): m_game(game), m_cellSize(cellSize), m_pen(Qt::gray)
+GameItem::GameItem(const Game& game, qreal cellSize): m_game(game), m_cellSize(cellSize), m_pen(Qt::gray)
 {
 
 }
 
 void GameItem::setCellSize(qreal cellSize)
 {
-    m_cellSize = cellSize;
+	m_cellSize = cellSize;
 }
 
 QRectF GameItem::boundingRect() const
 {
-    return QRectF(-10, -10, m_game.currentGrid().width() * m_cellSize + 12, m_game.currentGrid().height() * m_cellSize + 12);
+	return QRectF(-10, -10, m_game.currentGrid().width() * m_cellSize + 12, m_game.currentGrid().height() * m_cellSize + 12);
 }
 
-void GameItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+void GameItem::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget)
 {
-    auto &grid = m_game.currentGrid();
-    QRectF rect(0, 0, m_cellSize * m_game.currentGrid().width(), m_cellSize * m_game.currentGrid().height());
-    painter->drawImage(rect, m_game.currentGrid());
+	auto& grid = m_game.currentGrid();
+	QRectF rect(0, 0, m_cellSize * m_game.currentGrid().width(), m_cellSize * m_game.currentGrid().height());
+	painter->drawImage(rect, m_game.currentGrid());
+
+	//draw grid
+	for (int i = 0; i <= m_game.currentGrid().width(); i++)
+		painter->drawLine(i * m_cellSize, 0, i * m_cellSize, m_cellSize * m_game.currentGrid().height());
+
+	for (int i = 0; i <= m_game.currentGrid().height(); i++)
+		painter->drawLine(0, i * m_cellSize, m_cellSize * m_game.currentGrid().width(), i * m_cellSize);
 }
